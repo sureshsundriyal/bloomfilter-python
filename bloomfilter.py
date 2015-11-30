e783d83ca3223ecac4b33a9e1e5451d4ad86ffb8
Suresh Sundriyal <sureshsundriyal@gmail.com>
2015-11-30 00:28:16 -0800

    [bloomfilter] Fix up code for Python3

--- bloomfilter.py
+++ bloomfilter.py
@@ -1,5 +1,11 @@
 import math
 
+# Fixup for Python3
+try:
+    xrange
+except:
+    xrange = range
+
 _64_1 = int('1'*64, 2)
 
 def _hashlist(hash, num):
@@ -12,7 +18,7 @@ def _hashlist(hash, num):
                     lambda x: (l << 64) | (x*r),
                     xrange(1, num+1)
                 )
-            )
+           )
 
 class BloomFilter(object):
     def __init__(self, count=10000, error=0.01):
@@ -32,7 +38,7 @@ class BloomFilter(object):
 
         for hash in hashlist:
             x = hash % self.nbits
-            self.bitarray[x/8] |= 1<<(x%8)
+            self.bitarray[x//8] |= 1<<(x%8)
 
         self.items += 1
 
@@ -41,7 +47,10 @@ class BloomFilter(object):
 
         for hash in hashlist:
             x = hash % self.nbits
-            if not (self.bitarray[x/8] & 1<<(x%8)):
+            if not (self.bitarray[x//8] & 1<<(x%8)):
                 return False
 
         return True
+
+    def __contains__(self, hash):
+        return self.contains(hash)
